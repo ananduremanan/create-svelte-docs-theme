910169e2956dc230a2cef31d0c1be0b340230a45
anandhuremanan <ananduor@gmail.com>
2024-06-13 17:31:06 +0530

    Animations

--- site/src/lib/Card.svelte
+++ site/src/lib/Card.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-  class="flex flex-col shadow-lg rounded-xl border p-4 justify-center items-center"
+  class="flex flex-col shadow-lg rounded-xl border p-4 justify-center items-center hover:animate-bounce cursor-pointer"
 >
   <div class="text-lg md:text-2xl">{title}</div>
   <p class="text-xs mt-2">
